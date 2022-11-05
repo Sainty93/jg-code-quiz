0580a9e0f8ad6198018858c3b2cda7ab426fc1ef
Sainty93 <jenandfloyd93@icloud.com>
2022-11-05 10:22:18 -0700

    created questions array

---
+++ assets/questions.js
@@ -0,0 +1,24 @@
+
+
+var questions = [
+    {
+        question: "What is NOT a javascript data type?",
+        choices: ["string", "boolean", "alert", "number"],
+        answer: "alert"
+    },
+    {
+        question: "What is NOT a javascript data type?",
+        choices: ["string", "boolean", "alert", "number"],
+        answer: "alert"
+    },
+    {
+        question: "What is NOT a javascript data type?",
+        choices: ["string", "boolean", "alert", "number"],
+        answer: "alert"
+    },
+    {
+        question: "What is NOT a javascript data type?",
+        choices: ["string", "boolean", "alert", "number"],
+        answer: "alert"
+    }
+]
